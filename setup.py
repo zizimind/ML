5ad9778c86307ca8fa32584374470bac2e645ac3
zizimind <55913493+zizimind@users.noreply.github.com>
2021-04-10 22:00:14 +1000

    Gaussian and Binomial with unit testing

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions'],
+      zip_safe=False)
+
